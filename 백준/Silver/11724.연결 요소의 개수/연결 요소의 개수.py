27f7560feb38697055f3a30716d91a49d01975e9
Junseok Jang <jjs4502@naver.com>
2022-02-20 09:20:22 +0900

    Update 연결 요소의 개수.py
    설명 주석 추가

--- 백준/Silver/11724.연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724.연결 요소의 개수/연결 요소의 개수.py
@@ -8,11 +8,13 @@ visited=[0 for _ in range(a+1)]
 def dfs(x):
   visited[x]=1
   for i in array[x]:
+# 방문하지 않은 정점만 1로 만들면 되므로        
     if visited[i]==0:
       dfs(i)
   return True
 result=0
 for i in range(1,a+1):
+# 방문하지 않은 정점에 대하여
   if visited[i]==0:
     dfs(i)
     result+=1
