d2a03227aa1d43972332233fd9627c8b4ad6b6bd
Junseok Jang <jjs4502@naver.com>
2022-02-28 12:51:07 +0900

    Update 바이러스.py
    설명 주석 추가

--- 백준/Silver/2606.바이러스/바이러스.py
+++ 백준/Silver/2606.바이러스/바이러스.py
@@ -1,13 +1,17 @@
 a=int(input())
 b=int(input())
 array = [[] for _ in range(a+1)]
+# 인접 행렬 구현
 for _ in range(b):
     x, y = map(int, input().split())		
     array[x].append(y)
     array[y].append(x)
 visited=[0 for _ in range(a+1)]
+
 def dfs(array,x):
+  # 방문 표시
   visited[x]=1
+  # 인접한 정점들을 방문한뒤 방문 표시
   for i in array[x]:
     if visited[i]==0:
       dfs(array,i)
