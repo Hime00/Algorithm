a02182424fffb5918d4a0bc9e9a154f574328a80
Junseok Jang <jjs4502@naver.com>
2022-03-10 22:30:09 +0900

    Update 좌표 정렬하기 2.py

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -4,6 +4,7 @@ for i in range(n):
   x,y=map(int,input().split())
   array.append((x,y))
 array.sort(key=lambda x:x[0])
+# 우선순위가 y좌표이므로 마지막에 y좌표 우선정렬 한다
 array.sort(key=lambda x:x[1])
 for i in array:
   print(i[0],i[1])
